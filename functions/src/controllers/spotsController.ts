e1f18b500823c9932874edaddbad0c06030831ce
= <34914748+corentin-ach@users.noreply.github.com>
2021-11-10 09:48:52 +0100

    feat: :art: UpdateSpot function

--- functions/src/controllers/spotsController.ts
+++ functions/src/controllers/spotsController.ts
@@ -24,3 +24,29 @@ export const getAllSpots = async (req: Request, res: Response) => {
   }
 };
 
+export const updateSpot = async (req: Request, res: Response) => {
+  const {body: {quality}, params: {entryId}} = req;
+  try {
+    const spot = db.collection("spots").doc(entryId);
+    const currentSpot = (await spot.get()).data() || {};
+    const updatedSpot = {
+      name: currentSpot.name,
+      points: currentSpot.points,
+      quality: quality || currentSpot.quality,
+    };
+    await spot.set(updatedSpot).catch((error) => {
+      return res.status(400).json({
+        status: "error",
+        message: error.message,
+      });
+    });
+
+    return res.status(200).json({
+      status: "success",
+      message: "entry updated successfully",
+      data: updatedSpot,
+    });
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
 import express from "express";
 import {addEntry, getAllEntries} from "./entryController";
-import {getAllSpots} from "./controllers/spotsController";
+import {getAllSpots, updateSpot} from "./controllers/spotsController";
 
 const app = express();
 
@@ -9,5 +9,6 @@ app.get("/", (req, res) => res.status(200).send("Hey there!"));
 app.post("/entries", addEntry);
 app.get("/entries", getAllEntries);
 app.get("/spots", getAllSpots );
+app.get("/spot", updateSpot);
 
 exports.app = functions.https.onRequest(app);

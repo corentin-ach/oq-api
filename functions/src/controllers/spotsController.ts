eb59a85b9143bb119b2b8e45efc3f260e47dd69d
= <34914748+corentin-ach@users.noreply.github.com>
2021-11-23 13:38:01 +0100

    feat: :sparkles: GET spot

--- functions/src/controllers/spotsController.ts
+++ functions/src/controllers/spotsController.ts
@@ -24,6 +24,26 @@ export const getAllSpots = async (req: Request, res: Response) => {
   }
 };
 
+export const getSpot = async (req: Request, res: Response) => {
+  const {params: {entryId}} = req;
+  try {
+    const spot = db.collection("spots").doc(entryId);
+    const currentSpot = (await spot.get()).data() || {};
+    const formatedSpot = {
+      name: currentSpot.name,
+      coords: currentSpot.coords,
+      quality: currentSpot.quality,
+    };
+    return res.status(200).json({
+      status: "success",
+      message: "get spot  successfully",
+      data: formatedSpot,
+    });
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+};
+
 export const updateSpot = async (req: Request, res: Response) => {
   const {body: {name, coords, quality}, params: {entryId}} = req;
   try {
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 import express from "express";
-import {getAllSpots, updateSpot} from "./controllers/spotsController";
+import {getAllSpots, updateSpot, getSpot} from "./controllers/spotsController";
 import cors from "cors";
 
 const app = express();
@@ -10,5 +10,6 @@ app.use(cors());
 app.get("/", (req, res) => res.status(200).send("Hey there!"));
 app.get("/spots", getAllSpots );
 app.patch("/spot/:entryId", updateSpot);
+app.get("/spot/:entryId", getSpot);
 
 exports.app = functions.https.onRequest(app);

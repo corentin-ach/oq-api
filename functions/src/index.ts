a9248682786e7a6e79f56b05c351267f551a6905
= <=>
2021-11-25 21:43:37 +0100

    perf: :zap: helmet introduction

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,10 +2,12 @@ import * as functions from "firebase-functions";
 import express from "express";
 import {getAllSpots, updateSpot} from "./controllers/spotsController";
 import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
 
 app.get("/", (req, res) => res.status(200).send("Hey there!"));
 app.get("/spots", getAllSpots );

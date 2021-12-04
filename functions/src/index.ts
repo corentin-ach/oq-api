be1dbb45f47aba30279d23dcf6e617f3011b8f19
corentin-ach <>
2021-12-04 19:33:27 +0100

    feat: :sparkles: Route notion

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import {getAllSpots} from "./controllers/spots/getAllSpots";
 import {updateSpot} from "./controllers/spots/updateSpot";
 import {getSpot} from "./controllers/spots/getSpot";
+import {getNotion} from "./controllers/notion/getNotion";
 
 const app = express();
 
@@ -15,5 +16,6 @@ app.get("/", (req, res) => res.status(200).send("Hey there!"));
 app.get("/spots", getAllSpots );
 app.patch("/spot/:entryId", updateSpot);
 app.get("/spot/:entryId", getSpot);
+app.get("/notion", getNotion);
 
 exports.app = functions.https.onRequest(app);

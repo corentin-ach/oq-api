5a21ed939aa0232d9580759b8360a414d1b03f61
= <34914748+corentin-ach@users.noreply.github.com>
2022-01-24 15:03:00 +0100

    feat: :sparkles: Fonction createVote

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
 import * as functions from "firebase-functions";
 import express from "express";
 import {getAllSpots, updateSpot, getSpot} from "./controllers/spotsController";
+import {createVote} from "./controllers/votesController";
+
 import cors from "cors";
 
 const app = express();
@@ -11,5 +13,6 @@ app.get("/", (req, res) => res.status(200).send("Hey there!"));
 app.get("/spots", getAllSpots );
 app.patch("/spot/:entryId", updateSpot);
 app.get("/spot/:entryId", getSpot);
+app.post("/votes", createVote);
 
 exports.app = functions.https.onRequest(app);

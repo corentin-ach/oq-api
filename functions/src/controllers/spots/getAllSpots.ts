062c07672f0cea45d202ab9b1c3d527f7090b948
corentin-ach <corentin.ach@gmail.com>
2022-02-02 21:46:59 +0100

    feat: :sparkles: Status change with time

--- functions/src/controllers/spots/getAllSpots.ts
+++ functions/src/controllers/spots/getAllSpots.ts
@@ -1,17 +1,42 @@
+import dayjs from "dayjs";
 import {Response} from "express";
 import {db} from "../../config";
-import {Spot} from "../../types/spot";
+import {Quality, Spot} from "../../types/spot";
 
 type Request = {
     body: Spot,
     params: { entryId: string }
   }
 
+const computeStatus = (quality: Quality) => {
+  const lastDate = dayjs(quality.date);
+  const nowDate = dayjs(dayjs(new Date()).format("YYYY-MM-DD"));
+  const status = nowDate.diff(lastDate, "day") >= 7 ? false : true;
+  return status;
+};
+
 export const getAllSpots = async (req: Request, res: Response) => {
   try {
     const allSpots: Spot[] = [];
     const querySnapshot = await db.collection("spots").get();
-    querySnapshot.forEach((doc: any) => allSpots.push(doc.data()));
+
+    querySnapshot.forEach((doc: any) => {
+      const formatedSpot = {
+        id: doc.data().id,
+        name: doc.data().name,
+        coords: doc.data().coords,
+        quality: {
+          date: doc.data().quality.date,
+          water: computeStatus(doc.data().quality) ? true : false,
+          plastic: computeStatus(doc.data().quality) ? true : false,
+          seal: computeStatus(doc.data().quality) ? true : false,
+        },
+        status: computeStatus(doc.data().quality),
+        votes: doc.data().votes,
+      };
+      console.log(formatedSpot);
+      return allSpots.push(formatedSpot);
+    });
     return res.status(200).json(allSpots);
   } catch (error: any) {
     return res.status(500).json(error.message);
--- functions/src/controllers/spots/getSpot.ts
+++ functions/src/controllers/spots/getSpot.ts
@@ -1,11 +1,25 @@
+import dayjs from "dayjs";
 import {Response} from "express";
 import {db} from "../../config";
-import {Spot} from "../../types/spot";
+import {Quality, Spot} from "../../types/spot";
 
 type Request = {
     body: Spot,
     params: { entryId: string }
   }
+
+/*
+  Fonction dans quality :
+  si dans votes, le dernier vote date plus de 7 jours,
+  alors quality passe à true
+*/
+
+const computeStatus = (quality: Quality) => {
+  const lastDate = dayjs(quality.date);
+  const nowDate = dayjs(dayjs(new Date()).format("YYYY-MM-DD"));
+  const status = nowDate.diff(lastDate, "day") >= 2 ? false : true;
+  return status;
+};
 
 export const getSpot = async (req: Request, res: Response) => {
   const {params: {entryId}} = req;
@@ -16,7 +30,7 @@ export const getSpot = async (req: Request, res: Response) => {
       name: currentSpot.name,
       coords: currentSpot.coords,
       quality: currentSpot.quality,
-      status: currentSpot.status,
+      status: computeStatus(currentSpot.quality),
       votes: currentSpot.votes,
     };
     return res.status(200).json({
